583618b2a1a5baa9afe821685fe92dd158486041
MitchelRos <mrosado19@jaumebalmes.net>
2022-10-03 16:37:54 +0200

    ª

---
+++ src/main/java/net/Jaumebalmes/APIG/ApigApplication.java
@@ -0,0 +1,13 @@
+package net.Jaumebalmes.APIG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/Jaumebalmes/APIG/ApigApplicationTests.java
@@ -0,0 +1,13 @@
+package net.Jaumebalmes.APIG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
