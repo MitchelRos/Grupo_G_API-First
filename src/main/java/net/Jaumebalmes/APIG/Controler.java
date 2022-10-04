dc88cc94d35a3aaecd4317aaf8982518aa2b5e73
ksama22 <ksama22@jaumebalmes.net>
2022-10-04 17:27:33 +0200

    add home page

--- src/main/java/net/Jaumebalmes/APIG/Controler.java
+++ src/main/java/net/Jaumebalmes/APIG/Controler.java
@@ -13,10 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controler {
 	private ArrayList<Alumno> alumneList = new ArrayList<Alumno>();
 	
-	@GetMapping("/hello")
-	public String bonaTarda() {
-		return "<h1>bona tarda</h1>";
+	@GetMapping("/")
+	public String home() {
+		String html_page = "<!DOCTYPE html><html><head><title>Home</title></head><body>Examples paths<ul><li> <a href=\"/alumnes\">Alumnes</a></li><li> <a href=\"/alumne/1\">Alumnes:id</a> </li></ul></ul></body></html>";
+		return html_page;
 	}
+	
 	
 	
 	@GetMapping("alumnes")
@@ -37,7 +39,7 @@ public class Controler {
 		alumneList.add(new Alumno(1,"LLuc","Pirilla",LocalDate.of(2001, 3, 21)));
 		alumneList.add(new Alumno(2,"Kevin","Pirilla",LocalDate.of(199, 1, 19)));
 		alumneList.add(new Alumno(3,"Joan","Pirilla",LocalDate.of(1997, 4, 11)));
-		alumneList.add(new Alumno(4,"Raul","Pirilla",LocalDate.of(2002, 1, 29)));
+		alumneList.add(new Alumno(4,"Raul","Parejo",LocalDate.of(2002, 1, 29)));
 		alumneList.add(new Alumno(5,"Luis","Pirilla",LocalDate.of(1995, 3, 15)));
 		alumneList.add(new Alumno(6,"Mitchel","Pirilla",LocalDate.of(1992, 1, 20)));
 
