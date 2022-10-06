a4c4971b784edb2b8e44720dad3a3c52e60e8c4f
Kevin Sama 22 <113372511+ksama22@users.noreply.github.com>
2022-10-06 19:32:54 +0200

    New atributes alumnes and obj uf (#22)
    * testing new alumno OBJ
    
    * deprecated contructor & datelocaltime

--- src/main/java/net/Jaumebalmes/APIG/Alumno.java
+++ src/main/java/net/Jaumebalmes/APIG/Alumno.java
@@ -1,34 +1,52 @@
 package net.Jaumebalmes.APIG;
 
+import java.util.Date;
+import java.text.ParseException;
 import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.Month;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
 class Alumno{
-    private int id;
-    private String name;
-    private String surname;
-    private LocalDate birthDate;
-    
-    public Alumno() {
+	private int id;
+	private String name;
+	private String surname;
+	private LocalDateTime birthDate;
+	private LocalDateTime timeEntry;
+	private LocalDateTime timeExit;
+	private UF modul;
+
+
+	public Alumno() {
 		this.id = 0;
 		this.name = "Ejemplo";
 		this.surname = "Ejemplo";
-		this.birthDate = LocalDate.of(2022, 1, 11);
+		this.birthDate = LocalDateTime.now();
+		this.timeEntry = LocalDateTime.now();
+		this.timeExit = LocalDateTime.now();
+		this.modul = new UF();
+	}
+
+
+	public Alumno(int id, String name, String surname, LocalDateTime birthDate, LocalDateTime horaEntrada,
+			Date horaSortida, UF modul) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.surname = surname;
+		this.birthDate = birthDate;
+		this.timeEntry = timeEntry;
+		this.timeExit = timeExit;
+		this.modul = modul;
 	}
 
 	public Alumno(int id, String name, String surname, LocalDate birthDate) {
 		this.id = id;
 		this.name = name;
 		this.surname = surname;
-		this.birthDate = birthDate;
+		this.birthDate = LocalDateTime.now();
 	}
+
 	public int getId() {
 		return id;
 	}
@@ -47,13 +65,28 @@ class Alumno{
 	public void setSurname(String surname) {
 		this.surname = surname;
 	}
-	public LocalDate getBirthDate() {
+	public LocalDateTime getBirthDate() {
 		return birthDate;
 	}
-	public void setBirthDate(LocalDate birthDate) {
+	public void setBirthDate(LocalDateTime birthDate) {
 		this.birthDate = birthDate;
 	}
-    
-    
-    
+
+	private String getDateString(LocalDateTime myDateObj) {
+		//LocalDateTime myDateObj = LocalDateTime.now();  
+		DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");  
+		return myDateObj.format(myFormatObj);  
+		
+
+	}
+
+
+	@Override
+	public String toString() {
+		return "Alumno [id=" + id + ", name=" + name + ", surname=" + surname + ", birthDate=" + ""
+				+ ", timeEntry=" + getDateString(timeEntry) + ", timeExit=" + getDateString(timeExit) + ", modul=" + modul + "]";
+	}
+
+
+
 }
