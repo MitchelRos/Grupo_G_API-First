e6f4dae70116211c6d7c76794f434d2979b6561d
Kevin Sama 22 <113372511+ksama22@users.noreply.github.com>
2022-10-10 18:51:07 +0200

    refactor OBJ Alumne (Student) & alumnes per grup (#37)
    * refactor OBJ Alumne (Student) & alumnes per grup
    
    * change functions name

--- src/main/java/net/Jaumebalmes/APIG/Controler.java
+++ src/main/java/net/Jaumebalmes/APIG/Controler.java
@@ -3,21 +3,15 @@ package net.Jaumebalmes.APIG;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.HashMap;
-
-import java.nio.Buffer;
-import javax.naming.spi.DirStateFactory.Result;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controler {
-	private HashMap<Integer, Alumno> alumneList = new HashMap<>();
-	//private HashMap<Integer, UF> ufList = new HashMap<>();
+	private HashMap<Integer, Student> studentList = new HashMap<>();
+	// private HashMap<Integer, UF> ufList = new HashMap<>();
 
 	// Te muestra en web el html cuanod pones "..../" al final de local host
 	@GetMapping("/")
@@ -26,32 +20,46 @@ public class Controler {
 
 	}
 
-	//Te muestra cuando pones "..../alumnes" a toodos los alumnos
-	@GetMapping("alumnes")
-	public HashMap<Integer, Alumno> getAlumnos(){
-		alumneList = GenerateFakeData.generateAlumnes();
-		return alumneList;
+	// Te muestra cuando pones "..../students" a todos los alumnos
+	@GetMapping("students")
+	public HashMap<Integer, Student> getStudents() {
+		studentList = GenerateFakeData.generateStudents();
+		return studentList;
 	}
 
-	//Te muestra al poner un id al final del link el alumno en concreto
-	@GetMapping("alumne/{nId}")
-	public Alumno getAlumne(@PathVariable int nId){
-		alumneList = GenerateFakeData.generateAlumnes();	
-		return alumneList.get(nId);
+	@GetMapping("students/{mGroup}")
+	public HashMap<Integer, Student> getStudentsByGroup(@PathVariable String mGroup) {
+		studentList = GenerateFakeData.generateStudents();
+		// Lista auxiliar para devolver solo la query por grupo
+		HashMap<Integer, Student> groupAnimeList = new HashMap<>();
+		for (Student s : studentList.values()) {
+			// La busqueda se hace en mayuscula para evitar diferencias entre mayus y minus.
+			if ((s.getGrup().toUpperCase()).matches((mGroup.toUpperCase()))) {
+				groupAnimeList.put(s.getId(), s);
+			}
+		}
+		return groupAnimeList;
 	}
 
-	//Te devuelve el string del archivo que has puesto dentro cuando lo llamas
+	// Te muestra al poner un id al final del link el alumno en concreto
+	@GetMapping("student/{nId}")
+	public Student getStudent(@PathVariable int nId) {
+		studentList = GenerateFakeData.generateStudents();
+		return studentList.get(nId);
+	}
+
+	// Te devuelve el string del archivo que has puesto dentro cuando lo llamas
 	private String htmlString(String link) {
-		StringBuilder html=new StringBuilder();
-		String results="";
+		StringBuilder html = new StringBuilder();
+		String results = "";
 		try (FileReader flrd = new FileReader(link)) {
-			try(BufferedReader bfrd = new BufferedReader(flrd)){
+			try (BufferedReader bfrd = new BufferedReader(flrd)) {
 				String val;
-				while ((val=bfrd.readLine()) != null) {
+				while ((val = bfrd.readLine()) != null) {
 					html.append(val);
 				}
 				results = html.toString();
-			}catch (IOException e) {
+			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
@@ -62,14 +70,5 @@ public class Controler {
 		}
 		return results;
 	}
-
-
-
-
-
-
-
-
-
 
 }
