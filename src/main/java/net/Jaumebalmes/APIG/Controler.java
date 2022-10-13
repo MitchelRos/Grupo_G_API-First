4272883994e9d7ba2675d9d0d662d1d96f053253
MitchelRos <113449451+MitchelRos@users.noreply.github.com>
2022-10-13 16:42:41 +0200

    Añadir OBJ CURS (#38)

--- src/main/java/net/Jaumebalmes/APIG/Controler.java
+++ src/main/java/net/Jaumebalmes/APIG/Controler.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
